192a59a6f5fa18e295cf4804e56eb816d7586220
Maciej Kaznowski <maciej@kaznowski.com>
2023-10-21 15:25:35 +0100

    Add a Sigma Sports scraper project

---
+++ sigma_sports_scraper/src/main/kotlin/com/innercirclesoftware/sigmasportsscraper/SigmaSportsScraperApplication.kt
@@ -0,0 +1,11 @@
+package com.innercirclesoftware.sigmasportsscraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SigmaSportsScraperApplication
+
+fun main(args: Array<String>) {
+    runApplication<SigmaSportsScraperApplication>(*args)
+}

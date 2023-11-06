82f4f6192fb59e8ea1c23d00235eaa76f9677e75
Maciej Kaznowski <maciej@kaznowski.com>
2023-11-06 22:23:01 +0000

    Add Spring Data JPA and flyway

--- sigma_sports_scraper/src/main/kotlin/com/innercirclesoftware/sigmasportsscraper/SigmaSportsScraperApplication.kt
+++ sigma_sports_scraper/src/main/kotlin/com/innercirclesoftware/sigmasportsscraper/SigmaSportsScraperApplication.kt
@@ -2,8 +2,10 @@ package com.innercirclesoftware.sigmasportsscraper
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class SigmaSportsScraperApplication
 
 fun main(args: Array<String>) {
